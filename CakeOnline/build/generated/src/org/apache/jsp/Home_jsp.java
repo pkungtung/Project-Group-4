23b26cd6f76e003be248501e7f43d7707f1c112f
pkungtung <phungtung1993@gmail.com>
2015-09-20 23:32:13 +0700

    banner done

--- CakeOnline/build/generated/src/org/apache/jsp/Home_jsp.java
+++ CakeOnline/build/generated/src/org/apache/jsp/Home_jsp.java
@@ -11,10 +11,20 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   private static java.util.List<String> _jspx_dependants;
 
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_if_test;
+
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public java.util.List<String> getDependants() {
     return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_if_test = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_if_test.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -54,6 +64,10 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\" />\r\n");
       out.write("    </head>\r\n");
       out.write("    <body>\r\n");
+      out.write("        ");
+      if (_jspx_meth_c_if_0(_jspx_page_context))
+        return;
+      out.write("\r\n");
       out.write("\r\n");
       out.write("        ");
       org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "include/menu.jsp", out, false);
@@ -102,5 +116,35 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
     }
+  }
+
+  private boolean _jspx_meth_c_if_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:if
+    org.apache.taglibs.standard.tag.rt.core.IfTag _jspx_th_c_if_0 = (org.apache.taglibs.standard.tag.rt.core.IfTag) _jspx_tagPool_c_if_test.get(org.apache.taglibs.standard.tag.rt.core.IfTag.class);
+    _jspx_th_c_if_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_if_0.setParent(null);
+    _jspx_th_c_if_0.setTest(((java.lang.Boolean) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${param.login == 'success'}", java.lang.Boolean.class, (PageContext)_jspx_page_context, null)).booleanValue());
+    int _jspx_eval_c_if_0 = _jspx_th_c_if_0.doStartTag();
+    if (_jspx_eval_c_if_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      do {
+        out.write("\r\n");
+        out.write("        <script>\r\n");
+        out.write("            alert(\"dang nhap thanh cong\");\r\n");
+        out.write("        </script>\r\n");
+        out.write("        ");
+        int evalDoAfterBody = _jspx_th_c_if_0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+    }
+    if (_jspx_th_c_if_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+      return true;
+    }
+    _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+    return false;
   }
 }
--- CakeOnline/build/web/Client/include/menu.jsp
+++ CakeOnline/build/web/Client/include/menu.jsp
@@ -8,8 +8,29 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <c:set var="path" value="${pageContext.request.servletPath}" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script type="text/javascript" src="css/coin-slider.js"></script>
+<script src="../jquery-2.1.4.min.js" type="text/javascript"></script>
+<script type = "text/javascript" >
+    $(function () {
+        $('.slide img:gt(0)').hide();
+        setInterval(function () {
+            $('.slide :first-child').fadeOut()
+                    .next('img').fadeIn()
+                    .end().appendTo('.slide');
+        },
+                1500);
+    })
+</script>
+<style type="text/css">
+    .slide {
+        position:relative; width:940px; 
+        background: #aa6581;
+        padding: 10px;
+        height: 240px;
+    }
+    .slide img {position:absolute;}
+    .slide a img {position:absolute;}
+</style>
 <link rel="stylesheet" href="css/coin-slider-styles.css" type="text/css" />
 <div id="header">    
     <div>                
@@ -71,8 +92,14 @@
                 </ul>
             </c:otherwise>
         </c:choose>
-        <div class="section" style="height: 240px">
-            <a href="Home.jsp"><img src="images/wedding-cupcakes-small.jpg" height="100%" width="100%" alt="Image"/></a>
+        <div class="slide" style="height: 240px;">
+            <img src="images/1.jpg" width="960" height="240" />
+            <img src="images/2.jpg" width="960" height="240" />
+            <img src="images/3.jpg" width="960" height="240" />
+            <img src="images/4.jpg" width="960" height="240" />
+            <img src="images/5.jpg" width="960" height="240" />
+            <img src="images/6.jpg" width="960" height="240" />
+            <!--            <a href="Home.jsp"><img src="images/wedding-cupcakes-small.jpg" height="240" width="940" alt="Image"/></a>-->
         </div>
 
     </div>
