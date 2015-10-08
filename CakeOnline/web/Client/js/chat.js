3e925fefd854f41058589852da0ff60acd8e8cb6
muoi123 <muoinvb01588@fpt.edu.vn>
2015-10-09 03:06:25 +0700

    10  chat online2

--- CakeOnline/web/Client/js/chat.js
+++ CakeOnline/web/Client/js/chat.js
@@ -3,9 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-window._sbzq||function(e){e._sbzq=[];var t=e._sbzq;t.push(["_setAccount",28178]);
-var n=e.location.protocol=="https:"?"https:":"http:";
-var r=document.createElement("script");r.type="text/javascript";
-r.async=true;r.src=n+"//static.subiz.com/public/js/loader.js";
-var i=document.getElementsByTagName("script")[0];
-i.parentNode.insertBefore(r,i)}(window);
+//window._sbzq||function(e){e._sbzq=[];var t=e._sbzq;t.push(["_setAccount",28178]);
+//var n=e.location.protocol=="https:"?"https:":"http:";
+//var r=document.createElement("script");r.type="text/javascript";
+//r.async=true;r.src=n+"//static.subiz.com/public/js/loader.js";
+//var i=document.getElementsByTagName("script")[0];
+//i.parentNode.insertBefore(r,i)}(window);
+
+(function() {var _h1= document.getElementsByTagName('title')[0] || false;
+var product_name = ''; if(_h1){product_name= _h1.textContent || _h1.innerText;}var ga = document.createElement('script'); ga.type = 'text/javascript';
+ga.src = '//live.vnpgroup.net/js/web_client_box.php?hash=b5316435b5c63b486ef5d6bbc5c1c5c2&data=eyJzc29faWQiOjMxNzIyNjYsImhhc2giOiIxZmYxNGFiZDdmNmJmMGM0ZTc2N2M4ZGQ2ZjRmNTQ0MiJ9&pname='+product_name;
+var s = document.getElementsByTagName('script');s[0].parentNode.insertBefore(ga, s[0]);})();
